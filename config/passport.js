bf3bcb72609fcbd5464109839aa74394000e8680
Ravi <ravi@ariemtech.com>
2013-04-25 13:25:50 +0530

    Passport Local and Google strategy added

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 
 var mongoose = require('mongoose')
+  , LocalStrategy = require('passport-local').Strategy
   , FacebookStrategy = require('passport-facebook').Strategy
+  , GoogleStrategy = require('passport-google-oauth').Strategy
   , User = mongoose.model('User')
 
 module.exports = function (passport, config) {
@@ -16,6 +18,25 @@ module.exports = function (passport, config) {
       done(err, user)
     })
   })
+
+  // use local strategy
+  passport.use(new LocalStrategy({
+          usernameField: 'email',
+          passwordField: 'password'
+      },
+      function(email, password, done) {
+          User.findOne({ email: email }, function (err, user) {
+              if (err) { return done(err) }
+              if (!user) {
+                  return done(null, false, { message: 'Unknown user' })
+              }
+              if (!user.authenticate(password)) {
+                  return done(null, false, { message: 'Invalid password' })
+              }
+              return done(null, user)
+          })
+      }
+  ))
 
   // use facebook strategy
   passport.use(new FacebookStrategy({
@@ -44,4 +65,31 @@ module.exports = function (passport, config) {
       })
     }
   ))
+
+  // use google strategy
+  passport.use(new GoogleStrategy({
+          consumerKey: config.google.clientID,
+          consumerSecret: config.google.clientSecret,
+          callbackURL: config.google.callbackURL
+      },
+      function(accessToken, refreshToken, profile, done) {
+          User.findOne({ 'google.id': profile.id }, function (err, user) {
+              if (!user) {
+                  user = new User({
+                      name: profile.displayName
+                      , email: profile.emails[0].value
+                      , username: profile.username
+                      , provider: 'google'
+                      , google: profile._json
+                  })
+                  user.save(function (err) {
+                      if (err) console.log(err)
+                      return done(err, user)
+                  })
+              } else {
+                  return done(err, user)
+              }
+          })
+      }
+  ));
 }
--- config/routes.js
+++ config/routes.js
@@ -6,7 +6,7 @@
 var mongoose = require('mongoose')
   , middlewares = require('./middlewares')
 
-module.exports = function (app, passport, auth) {
+module.exports = function (app, passport) {
 
   var home = require('../app/controllers/home')
   app.get('/', home.index)
